16a0c59fef0e71e5c53b10b2b4b8d1f54455d76b
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2019-02-19 12:21:34 -0700

    Add project files.

---
+++ RazorPagesWalkthrough.Web/Helpers/DataGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesWalkthrough.Web.Helpers
+{
+    /// <summary>
+    /// This class is not intended as part of the walkthrough.  Rather, it is used to generate a
+    /// sample set of board games.
+    /// </summary>
+    public static class DataGenerator
+    {
+    }
+}
---
+++ RazorPagesWalkthrough.Web/Models/BoardGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesWalkthrough.Web.Models
+{
+    public class BoardGame
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string PublishingCompany { get; set; }
+        public int MinPlayers { get; set; }
+        public int MaxPlayers { get; set; }
+    }
+}
